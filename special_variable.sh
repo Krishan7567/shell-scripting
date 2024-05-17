f61c8784b28180dd1fbfc4044f8052c4f97acd38
unknown <krishanb281988@gmail.com>
2024-05-17 11:08:28 +0530

    hello

--- special_variable.sh
+++ special_variable.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-read -p "enter your name name"
-echo -e "ur name is $name"
+
+#read -p "enter your name name"
+#echo -e "ur name is $name"
 
 
-#read -p 'Enter your name:' NAME 
-#echo -e "Name of the executed user is $NAME"
+read -p 'Enter your name:' NAME 
+echo -e "Name of the executed user is $NAME"
