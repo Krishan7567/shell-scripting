162ce29d4ad4bd847a7f21e987070c9330b4d934
unknown <krishanb281988@gmail.com>
2024-05-17 11:39:28 +0530

    hello

---
+++ function.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+stat()
+{
+   echo -e "load avg is : $(uptime | awk -F : '{print $NF}'|awk -F , '{print $3}')" 
+   echo "number of user logged in : $(who | wc -l)"
+}
+
+calling stat fuc
+
+stat
