d8a8fe9f5683c2fe72a3971ff53ceeef81a92cf2
unknown <krishanb281988@gmail.com>
2024-05-19 16:04:53 +0530

    hello

--- if.sh
+++ if.sh
@@ -6,3 +6,14 @@ echo "correct"
 else 
 echo "in-correct"
 fi
+
+b=$1
+if [ "$b" == "kk"];then
+echo "yes"
+elif [ "$b" == "k" ];then
+echo "no"
+elif [ "$b" == "kkb"];then
+echo "you are best"
+if
+
+fi
