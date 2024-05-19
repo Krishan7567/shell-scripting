7184964cfef086ae32a27201044eec6ddadc89ba
unknown <krishanb281988@gmail.com>
2024-05-19 16:16:01 +0530

    hello

--- if.sh
+++ if.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-b=$1
-if [ "$b" == "kk"];then
-echo "yes"
-elif [ "$b" == "k" ];then
-echo "no"
-elif [ "$b" == "kkb"];then
-echo "you are best"
+#b=$1
+#if [ "$b" == "kk"];then
+#echo "yes"
+#elif [ "$b" == "k" ];then
+#echo "no"
+#elif [ "$b" == "kkb"];then
+#echo "you are best"
+#else 
+#echo "Value is not kk or kkb or k"
+#fi
+
+c=$1 
+
+if [ "$c" -eq "10" ] ; then 
+    echo "value of c is 10"
+
+elif [ "$c" -eq "20" ] ; then 
+    echo "value of c is 20"
+
+elif [ "$c" -eq "30" ] ; then 
+    echo "value of c is 30"
+
 else 
-echo "Value is not kk or kkb or k"
-fi
+    echo "Value is not 10 or 20 or 30"
+
+fi 
