0386df24839be2e8f8755593f5905b155ebe518f
unknown <krishanb281988@gmail.com>
2024-05-19 14:57:50 +0530

    hello

---
+++ 09case.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+Action=$1
+case $Action in
+
+start)
+     echo "start services"
+     exit 0
+     ;;
+stop)
+     echo "stop services"
+     exit 1
+     ;;
+*)
+    echo -e "\e[32m enter valid option\e[0m"
+
+esac
