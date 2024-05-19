3fc120895e1f0f163d1aa2be028d409c3e89119b
unknown <krishanb281988@gmail.com>
2024-05-19 16:21:59 +0530

    hello

--- if.sh
+++ if.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 b=$1
-if [ "$b" == "kk"] : then
+if [ "$b" == "kk"] ; then
+
     echo "yes"
-elif [ "$b" == "k" ] : then
+
+elif [ "$b" == "k" ] ; then
+
     echo "no"
+
 elif [ "$b" == "kkb"] ; then
+
     echo "you are best"
+
 else 
+
     echo "Value is not kk or kkb or k"
+    
 fi
 
 #c=$1 
