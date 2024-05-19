4f8ef65aa0482fb906cfa193971201694a2e99dc
unknown <krishanb281988@gmail.com>
2024-05-19 15:52:20 +0530

    hello

--- if.sh
+++ if.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-#a="abc"
-#if ["$a" == "abc"]; then
-#    echo "correct"
-#else
-#    echo "incorrect"
-#fi
-
-a="abcd" 
-
-if  [ "$a" == "abc" ]; then 
-    echo -e "\e[32m Both of the are equal \e[0m"
-
-fi 
+a="abcd"
+if ["$a" == "abcd"];then
+echo "correct"
+fi
